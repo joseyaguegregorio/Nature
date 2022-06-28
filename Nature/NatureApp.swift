4bf4134b00d0e4b11e824a971c2b40857fd9874d
joseyaguegregorio <joseyaguegregorio@gmail.com>
2022-06-28 20:33:32 +0200

    0.0.0

---
+++ Nature/NatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureApp.swift
+//  Nature
+//
+//  Created by STIdea on 28/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
